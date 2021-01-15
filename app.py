4f358f1d14b6e99e6b269afde1181770b3048dab
hunghly <hunghly@gmail.com>
2021-01-14 23:53:04 -0600

    updated displaying posts

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
+
+def get_db_connection():
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row #db connection returns rows like a python dictionary
+    return connection
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    """ Retrieve the posts from the db connection and pass it as part of render """
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * from posts').fetchall()
+    connection.close()
+    return render_template('index.html', posts=posts)
+
